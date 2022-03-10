03a030b4cf1aa724affa81581c2a28668684ea5a
Nathan Yee <yee.nathan.c@gmail.com>
2022-03-09 23:55:07 -0800

    updated all

--- sort.py
+++ sort.py
@@ -1,9 +1,10 @@
 def sort_list(input_list: list) -> list:
     i: int = 0
+    n: int = len(input_list)
     # bubble sort, after each loop the highest would bubble to the top and not be considered for the next loop
-    while i < len(input_list):
+    while i < n:
         j: int = 0
-        while j < len(input_list) - i - 1:
+        while j < n - i - 1:
             if type(input_list[j]) != type(input_list[j + 1]):  # bad input, types are not equal
                 return []
             if input_list[j] > input_list[j + 1]:
