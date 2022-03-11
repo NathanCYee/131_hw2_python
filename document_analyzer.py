ffb6f9d392992fe0e31bdd1eb8b9d3d4963bfe67
Nathan Yee <yee.nathan.c@gmail.com>
2022-03-10 23:54:10 -0800

    updated document_analyzer

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 def count_words(input_str: str) -> dict:
-    diced: list[str] = input_str.split()  # get the individual words to count
+    diced: List[str] = input_str.split()  # get the individual words to count
     counted_words: dict = {}
     for word in diced:
         if word in counted_words:  # increase the count
@@ -14,5 +17,5 @@ with open('document.txt') as f:  # open and close the file safely
     print("")  # format the output
     # sort from most to least first(negative) then by lexicographic order in tuple
     sorted_words = sorted(vals.items(), key=lambda val: (-val[1], val[0]))
-    for i in range(0, 5):
+    for i in range(min(5, len(sorted_words))):  # print the 5 most or all available if less than 5
         print(f"{sorted_words[i][0]}: {sorted_words[i][1]}")
